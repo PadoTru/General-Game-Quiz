935d8d3a72a16bf2d27aeb1dc70ce54baaee3697
PadoTru <81660990+PadoTru@users.noreply.github.com>
2021-05-27 00:30:28 +1200

    Add files via upload

---
+++ Instructions/Instructions.V1.py
@@ -0,0 +1,7 @@
+#Ask for user input
+instructions = input("Would you like to read the instructions?")
+#Print intructions
+print("The rules are simple. Answer the multi choice questions by entering a,b,c,d.")
+
+
+
