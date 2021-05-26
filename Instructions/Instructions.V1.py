85938a07e51d229d80564f377b8807ffce91f462
PadoTru <81660990+PadoTru@users.noreply.github.com>
2021-05-27 00:36:12 +1200

    Delete Instructions directory

--- Instructions/Instructions.V1.py
+++
@@ -1,7 +0,0 @@
-#Ask for user input
-instructions = input("Would you like to read the instructions?")
-#Print intructions
-print("The rules are simple. Answer the multi choice questions by entering a,b,c,d.")
-
-
-
