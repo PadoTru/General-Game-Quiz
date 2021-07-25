c2be60517cab97ed1aa6bec07c14a486219ef536
PadoTru <81660990+PadoTru@users.noreply.github.com>
2021-07-26 10:47:45 +1200

    Update Assembled Quiz.VersionFinal.py

--- Python Assessment/Assembled Quiz/Assembled Quiz.VersionFinal.py
+++ Python Assessment/Assembled Quiz/Assembled Quiz.VersionFinal.py
@@ -1,24 +1,64 @@
 from random import shuffle
 
+#variables 
+index = 0
+score = 0
+optnum = 0
+
 #lines used for aesthetics
 lines1 = ('===============================')
 lines2 = ('=========================================================')
 lines3 = ('===============================================================================')
 
-#introduction and disclaimer
-print("INTRO\nTHIS IS A GENERAL KNOWLEDGE GAME QUIZ MADE FOR MY SCHOOL PROJECT. \nIT IS MADE TO TEST YOUR GENERAL KNOWLEDGE ON ALL TYPES OF GAMES AND IS NOT INTENDED TO OFFEND ANY GROUPS OF PEOPLE, CULTURES, VALUES ETC.\nIF THERE ARE ANY PROBLEMS YOU CAN CONTACT ME AT: 19231@students.mrgs.school.nz\nPLEASE HAVE AN ENJOYABLE EXPERIENCE AND THANK YOU FOR TAKING THE TIME TO TRY OUT MY PROGRAM!")
-print(lines3+lines2,"\nLet's Begin!")
+#using dictionaries in order to store the questions, choices, and answers
+quiz = [
+["=What is the best selling video game of all time?=",
+ {'answer' : 'b', 'choice' : ' \na. Tetris \nb. Minecraft \nc. GTA V \nd. Call of Duty \n'}
+ ],
+["=Which of these consoles is the highest selling to date?=",
+ {'answer' : 'a', 'choice' : ' \na. PS2 \nb. Nintendo DS \nc. PS4 \nd. Xbox\n'}
+ ],
+["=How many squares on a Chess board?=",
+ {'answer' : 'c', 'choice' : ' \na. 49 \nb. 100 \nc. 64 \nd. 81\n'}
+ ],
+["=How many cards in a full deck of regular playing cards including the jokers?=",
+ {'answer' : 'a', 'choice' : ' \na. 54 \nb. 52 \nc. 50 \nd. 48\n'}
+ ],
+["=Which of these are not a starter pokemon?=",
+ {'answer' : 'd', 'choice' : ' \na. chimchar \nb. froakie \nc. bulbasaur \nd. charizard\n'}
+ ],
+["=Which game did Mario first appear in?=",
+ {'answer' : 'c', 'choice' : ' \na. Mario Kart \nb. Super Mario \nc. Donkey Kong \nd. Apex Legends\n'}
+ ],
+["=Which of these is not the colour of one of the 4 pacman ghosts?=",
+ {'answer' : 'c', 'choice' : ' \na. red \nb. orange \nc. yellow \nd. blue\n'}
+ ],
+["=What colour is the Pacman ghost named 'Blinky'?=",
+ {'answer' : 'a', 'choice' : ' \na. red \nb. orange \nc. yellow \nd. blue\n'}
+ ],
+["=What year was overwatch released?=",
+ {'answer' : 'd', 'choice' : ' \na. 2015 \nb. 2014 \nc. 2018 \nd. 2016\n'}
+ ],
+["=What is the name of the first ever video game?=",
+ {'answer' : 'b', 'choice' : ' \na. Tetris \nb. Pong \nc. Space Invaders \nd. Pacman\n'}
+ ],
+]
 
+#using a function to print introduction and disclaimer
+def intro():
+    print("INTRO\nTHIS IS A GENERAL KNOWLEDGE GAME QUIZ MADE FOR MY SCHOOL PROJECT. \nIT IS MADE TO TEST YOUR GENERAL KNOWLEDGE ON ALL TYPES OF GAMES AND IS NOT INTENDED TO OFFEND ANY GROUPS OF PEOPLE, CULTURES, VALUES ETC.\nIF THERE ARE ANY PROBLEMS YOU CAN CONTACT ME AT: 19231@students.mrgs.school.nz\nPLEASE HAVE AN ENJOYABLE EXPERIENCE AND THANK YOU FOR TAKING THE TIME TO TRY OUT MY PROGRAM!")
+    print(lines3+lines2,"\nLet's Begin!")
+    
 #using a function to ask for the users name, the variable will be name
 def user_details():
-            print(lines2,"\nHello", name, ". Welcome to Hafidz's General Game Quiz!\n"+lines2)
-while True:
-    name = input("Please Enter Your First Name: ").capitalize()
-    if name.isalpha():
-        break
+    global name
+    while True:
+        name = input("Please Enter Your First Name: ").capitalize()
+        if name.isalpha():
+            break
 
-    else:
-        print("\n"+lines1, "\nPlease Enter Only Letters\n"+lines1,"\n")
+        else:
+            print("\n"+lines1, "\nPlease Enter Only Letters\n"+lines1,"\n")
        
 #using a function to ask if the user would like to read insturcionts, the variable will be inst
 def instructions():
@@ -59,59 +99,35 @@ def rounds():
 
     total = r
 
-#using dictionaries in order to store the questions, choices, and answers
-quiz = [
-["=What is the best selling video game of all time?=",
- {'answer' : 'b', 'choice' : ' \na. Tetris \nb. Minecraft \nc. GTA V \nd. Call of Duty \n'}
- ],
-["=Which of these consoles is the highest selling to date?=",
- {'answer' : 'a', 'choice' : ' \na. PS2 \nb. Nintendo DS \nc. PS4 \nd. Xbox\n'}
- ],
-["=How many squares on a Chess board?=",
- {'answer' : 'c', 'choice' : ' \na. 49 \nb. 100 \nc. 64 \nd. 81\n'}
- ],
-["=How many cards in a full deck of regular playing cards including the jokers?=",
- {'answer' : 'a', 'choice' : ' \na. 54 \nb. 52 \nc. 50 \nd. 48\n'}
- ],
-["=Which of these are not a starter pokemon?=",
- {'answer' : 'd', 'choice' : ' \na. chimchar \nb. froakie \nc. bulbasaur \nd. charizard\n'}
- ],
-["=Which game did Mario first appear in?=",
- {'answer' : 'c', 'choice' : ' \na. Mario Kart \nb. Super Mario \nc. Donkey Kong \nd. Apex Legends\n'}
- ],
-["=Which of these is not the colour of one of the 4 pacman ghosts?=",
- {'answer' : 'c', 'choice' : ' \na. red \nb. orange \nc. yellow \nd. blue\n'}
- ],
-["=What colour is the Pacman ghost named 'Blinky'?=",
- {'answer' : 'a', 'choice' : ' \na. red \nb. orange \nc. yellow \nd. blue\n'}
- ],
-["=What year was overwatch released?=",
- {'answer' : 'd', 'choice' : ' \na. 2015 \nb. 2014 \nc. 2018 \nd. 2016\n'}
- ],
-["=What is the name of the first ever video game?=",
- {'answer' : 'b', 'choice' : ' \na. Tetris \nb. Pong \nc. Space Invaders \nd. Pacman\n'}
- ],
-]
 
-#shuffling the dictionary
-shuffle(quiz)
-
-#setting everything necessary to 0
-index = 0
-score = 0
-optnum = 0
+#asking for feedback using a function, the variable will be f
+def feedback():
+  while True:
+    try:
+      f = int(input("How would you rate the quiz from 1-5?: "))
+      if 0<f<=5:
+        break
+      else:
+        print("\n"+lines1,"\nPlease enter numbers 1-5 only\n"+lines1)
+        
+    except:
+      print("\n"+lines1,'\nPlease enter numbers only\n'+lines1)
 
 #calling all the functions
+intro()
 user_details()
 instructions()
 status()
 rounds()
+#shuffling the dictionary
+shuffle(quiz)
 
 #the actual quiz game starts
-print("\n\n                                                "+lines1+lines1,"\n                                                                 HAFIDZ'S GENERAL GAME QUIZ\n                                                "+lines1+lines1)
+print("\n\n\n                                                "+lines1+lines1,"\n                                                                 HAFIDZ'S GENERAL GAME QUIZ\n                                                "+lines1+lines1)
 print("(NOTE: At any time during the quiz, you can type 'xxx' to immediately exit the program)")
 print("QUESTIONS")
 
+#main routine
 while r>0:
     data = quiz[0]
     q = data[0]
@@ -159,19 +175,6 @@ print("\nYou have succesfully completed Hafidz's Game Quiz!")
 print("Congratulations!",name+", your final score is", score,"out of",total)
 print("That means you answered", (round(score/total*100,2)),"% of the questions correctly!")
 print("\nThanks for playing")
-
-#asking for feedback using a function, the variable will be f
-def feedback():
-  while True:
-    try:
-      f = int(input("How would you rate the quiz from 1-5?: "))
-      if 0<f<=5:
-        break
-      else:
-        print("\n"+lines1,"\nPlease enter numbers 1-5 only\n"+lines1)
-        
-    except:
-      print("\n"+lines1,'\nPlease enter numbers only\n'+lines1)
 feedback()
             
 print("Thank you for the feedback!")
